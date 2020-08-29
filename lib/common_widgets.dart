f1dc4d3783b7b4ca53c10d9874c14757d512aee5
Daven Casia <davencasia@gmail.com>
2020-08-29 13:42:08 -0600

    Move list to a single widget

--- lib/common_widgets.dart
+++ lib/common_widgets.dart
@@ -1,11 +1,15 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown/markdown.dart' as md;
 import 'package:provider/provider.dart';
+
 import 'package:goryon/viewmodels.dart';
 
 import 'models.dart';
 import 'screens/discover.dart';
 import 'screens/follow.dart';
+import 'screens/newtwt.dart';
 import 'screens/timeline.dart';
 
 class Avatar extends StatelessWidget {
@@ -116,6 +120,84 @@ class AppDrawer extends StatelessWidget {
               Navigator.of(context).pop();
               Navigator.of(context).pushReplacementNamed(routePath);
             },
+    );
+  }
+}
+
+class PostList extends StatelessWidget {
+  final List<Twt> twts;
+  final Function fetchNewPost;
+
+  const PostList({
+    Key key,
+    @required this.twts,
+    @required this.fetchNewPost,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (_, idx) {
+              final twt = twts[idx];
+              return ListTile(
+                isThreeLine: true,
+                leading: Avatar(imageUrl: twt.twter.avatar.toString()),
+                title: Text(
+                  twt.twter.nick,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: MarkdownBody(
+                        styleSheet: MarkdownStyleSheet(textScaleFactor: 1.2),
+                        onTapLink: (link) {
+                          print(link);
+                        },
+                        data: twt.sanitizedTxt,
+                        extensionSet: md.ExtensionSet.gitHubWeb,
+                      ),
+                    ),
+                    Divider(height: 0),
+                    ButtonTheme.fromButtonThemeData(
+                      data: Theme.of(context).buttonTheme.copyWith(
+                            minWidth: 0,
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                          ),
+                      child: FlatButton(
+                        onPressed: () async {
+                          if (await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => NewTwt(initialText: ''
+                                      // twt.replyText(user.username),
+                                      ),
+                                ),
+                              ) ??
+                              false) {
+                            fetchNewPost();
+                          }
+                        },
+                        child: Text(
+                          "Reply",
+                          style: Theme.of(context).textTheme.button,
+                        ),
+                      ),
+                    ),
+                    Divider(height: 0),
+                  ],
+                ),
+              );
+            },
+            childCount: twts.length,
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/screens/timeline.dart
+++ lib/screens/timeline.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
-import 'package:markdown/markdown.dart' as md;
 
 import '../common_widgets.dart';
 import '../models.dart';
@@ -62,71 +60,12 @@ class _TimelineState extends State<Timeline> {
               child: CircularProgressIndicator(),
             );
           }
-          final twts = timelineViewModel.twts;
 
           return RefreshIndicator(
             onRefresh: timelineViewModel.refreshPost,
-            child: ListView.separated(
-              padding: const EdgeInsets.only(bottom: 16),
-              separatorBuilder: (context, index) {
-                return Divider();
-              },
-              itemCount: twts.length,
-              itemBuilder: (context, index) {
-                final twt = twts[index];
-                return ListTile(
-                  isThreeLine: true,
-                  leading: Avatar(imageUrl: twt.twter.avatar.toString()),
-                  title: Text(
-                    twt.twter.nick,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: MarkdownBody(
-                          styleSheet: MarkdownStyleSheet(textScaleFactor: 1.2),
-                          onTapLink: (link) {
-                            print(link);
-                          },
-                          data: twt.sanitizedTxt,
-                          extensionSet: md.ExtensionSet.gitHubWeb,
-                        ),
-                      ),
-                      Divider(),
-                      Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () async {
-                              if (await Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => NewTwt(
-                                        initialText:
-                                            twt.replyText(user.username),
-                                      ),
-                                    ),
-                                  ) ??
-                                  false) {
-                                timelineViewModel.fetchNewPost();
-                              }
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(4),
-                              child: Text(
-                                "Reply",
-                                style: Theme.of(context).textTheme.button,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
+            child: PostList(
+              fetchNewPost: timelineViewModel.fetchNewPost,
+              twts: timelineViewModel.twts,
             ),
           );
         },
